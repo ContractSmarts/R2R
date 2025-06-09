d8e42cbb6c3f50e315ce1c036b6fac32f3954d8b
David Whiting <david.g.whiting@gmail.com>
2025-06-09 22:17:48 +0000

    saving before rerunning to remove tls

--- project/startup.sh
+++ project/startup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # 1. Start postgres first
-docker compose --profile step1 up -d
-docker compose --profile step1 wait postgres
+docker compose --profile postgres up -d
+docker compose --profile postgres wait postgres
 
 # 2. Run setup script directly in bash 
 # (decision to run internal in script)
@@ -12,11 +12,11 @@ docker compose --profile step1 wait postgres
 docker compose --profile step2 up -d
 
 # 4. Start hatchet-migration , hatchet-setup-config, etc. 
-docker compose --profile step1 --profile step2 --profile setup up -d
+docker compose --profile postgres --profile step2 --profile hatchet up -d
 
 # 5. Start hatchet-engine and hatchet-engine-dashboard
-docker compose --profile step1 --profile step2 --profile step3 up -d
+docker compose --profile postgres --profile step2 --profile hatchet --profile r2r up -d
 
-# 6. Start r2r
-docker compose --profile step1 --profile setup --profile step2 --profile step3 --profile step4 up -d
+## 6. Start r2r
+#docker compose --profile step1 --profile setup --profile step2 --profile step3 --profile step4 up -d
 
