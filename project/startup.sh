d4d9dd8043f50cce40ef802a0cacbf9c21678c40
David Whiting <david.g.whiting@gmail.com>
2025-06-10 02:02:47 +0000

    last commit before bailing on hatchet

--- project/startup.sh
+++ project/startup.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 # 1. Start postgres first
-docker compose --profile postgres up -d
-docker compose --profile postgres wait postgres
+#docker compose --profile postgres up -d
+#docker compose --profile postgres wait postgres
+
+docker compose up -d postgres
 
 # 2. Run setup script directly in bash 
 # (decision to run internal in script)
 ./scripts/setup-db.sh
 
 # 3. Start step2 services: graph cluster, unstructured, minio, and rabbitmq
-docker compose --profile step2 up -d
+docker compose up -d minio graph_clustering unstructured hatchet-rabbitmq
 
 # 4. Start hatchet-migration , hatchet-setup-config, etc. 
 docker compose --profile postgres --profile step2 --profile hatchet up -d
